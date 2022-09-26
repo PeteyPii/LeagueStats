b60253b0d5daacac2d5e2ff9027c9e899d25f669
Patrick Wrobel <me@patrickwrobel.dev>
2022-09-25 23:56:06 -0400

    Fixed kdas command

--- lol/commands/champ_kdas.py
+++ lol/commands/champ_kdas.py
@@ -5,6 +5,7 @@ import datapipelines
 from lol import command
 from lol.flags.match_filtering import MatchFilteringFlags
 from lol.flags.table_output import TableOutputFlags
+from lol.flags.region import RegionFlag
 
 
 class ChampionKdasCommand(command.Command):
@@ -13,6 +14,7 @@ class ChampionKdasCommand(command.Command):
     super().__init__(name)
     self.match_filtering_flags = MatchFilteringFlags(self)
     self.table_output_flags = TableOutputFlags(self)
+    self.region_flag = RegionFlag(self)
 
   def help_message(self):
     return (f'Usage: {self._PROGRAM} {self.name} <summoner_names>\n'
@@ -37,7 +39,7 @@ class ChampionKdasCommand(command.Command):
         print('Summoner name cannot be empty.')
         return
       try:
-        summoners.append(cass.Summoner(name=name).load())
+        summoners.append(cass.Summoner(name=name, region=self.region_flag.value).load())
       except datapipelines.common.NotFoundError:
         print(f'Summoner "{name}" not found.')
         return
@@ -46,9 +48,9 @@ class ChampionKdasCommand(command.Command):
     pipeline = self.match_filtering_flags.filter_steps() + [
         {'$project': {'participants': True}},
         {'$unwind': '$participants'},
-        {'$match': {'participants.accountId': {'$in': [summoner.account_id for summoner in summoners]}}},
+        {'$match': {'participants.puuid': {'$in': [summoner.puuid for summoner in summoners]}}},
         {'$group': {'_id': {'championId': '$participants.championId',
-                            'accountId': '$participants.accountId'},
+                            'puuid': '$participants.puuid'},
                     'games_played': {'$sum': 1},
                     'kills': {'$sum': '$participants.stats.kills'},
                     'deaths': {'$sum': '$participants.stats.deaths'},
@@ -57,8 +59,9 @@ class ChampionKdasCommand(command.Command):
         {'$addFields': {'kda': {'$divide': [{'$add': ['$kills', '$assists']},
                                             {'$cond': [{'$lte': ['$deaths', 0]}, 1, '$deaths']}]}}},
     ]  # yapf: disable
-    results = {(result['_id']['championId'], result['_id']['accountId']): result
-               for result in self.db.matches.aggregate(pipeline)}
+    results = {
+        (result['_id']['championId'], result['_id']['puuid']): result for result in self.db.matches.aggregate(pipeline)
+    }
 
     global_pipeline = self.match_filtering_flags.filter_steps() + [
         {'$project': {'participants': True}},
@@ -74,14 +77,14 @@ class ChampionKdasCommand(command.Command):
     ]  # yapf: disable
     global_results = {result['_id']['championId']: result for result in self.db.matches.aggregate(global_pipeline)}
 
-    champion_list = cass.get_champions()
+    champion_list = cass.get_champions(region=self.region_flag.value)
     champ_id_to_name = {champ.id: champ.name for champ in champion_list}
 
     table = []
     for champ_id, champ_name in sorted(champ_id_to_name.items(), key=lambda t: t[1]):
       row = collections.OrderedDict({'Champion': champ_name})
       for summoner in summoners:
-        row[summoner.name] = self.format_result(results.get((champ_id, summoner.account_id)))
+        row[summoner.name] = self.format_result(results.get((champ_id, summoner.puuid)))
       row['Global Avg'] = self.format_result(global_results.get(champ_id))
       table.append(row)
     self.table_output_flags.output_table(table)
